856838641879481f53e22c01c7aadbb88171b2bd
Amir <amir@Amirs-Mac.local>
2018-07-15 02:54:34 -0700

    Initial Commit

---
+++ photoList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  photoList
+//
+//  Created by Amir on 7/15/2016.
+//  Copyright © 2018 uni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
